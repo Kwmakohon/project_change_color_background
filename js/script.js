192b425e6055b7bbaa3fb1d6a085504fb81177c3
Kristen Makohon <kristen@Kristens-MBP.attlocal.net>
2020-04-16 15:42:49 -0400

    Made my own code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+//Define Variables, Array
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const colors = ['#F0FFFF', '#00BFFF', '#CD5C5C', '#F08080', '#E0FFFF', '#B0C4DE'];
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+//Listen for the click, what does it do?
+body.style.backgroundColor = '#FFFAF0';
+button.addEventListener('click', () => {body.style.backgroundColor = colors[Math.floor((Math.random() * colors.length))]})
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
-}
+
+
+
